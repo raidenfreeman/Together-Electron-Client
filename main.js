b0d78b2fb56ebf073082cb8c91b28ca84aea1f1d
Raiden Freeman <raidenfreeman@hotmail.com>
2017-08-29 22:58:00 +0300

    added support for vlc not on path

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ var app = require('app');  // Module to control application life.
 var path = require('path');
 
 
-var cmd=require('node-cmd');
+var cmd = require('node-cmd');
 
 var cp = require('child_process')
 var vlcCommand = require('vlc-command')
 
-exports.startVLC = file => {  
-    console.log('FILE: ',file);
-var directory = path.dirname(file).replace(/\s/g, '');
-console.log('DIR: |'+directory+'|');
-var gg = '--http-src '+directory;
-console.log(gg);
+exports.startVLC = file => {
+  console.log('FILE: ', file);
+  var directory = path.dirname(file).replace(/\s/g, '');
+  console.log('DIR: |' + directory + '|');
+  var gg = '--http-src ' + directory;
+  console.log(gg);
   // vlcCommand(function (err, cmd) {
   //   if (err) return console.error('could not find vlc command path')
   //   console.log(cmd);
@@ -31,12 +31,24 @@ console.log(gg);
   //     })
   //   }
   // })
-    // console.log('vlc ', directory ,' --http-password 1234');
-    var g = '"D:\Downloads\Game.of.Thrones.S07E05.Eastwatch.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT[ettv]\Game.of.Thrones.S07E05.Eastwatch.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT[ettv].mkv"';
-    // cmd.run('vlc --extraintf http --http-src '+directory+' --http-password 1234');
-    cmd.run('vlc --extraintf http --http-password 1234');
-//    'vlc --extraintf http --http-src D:\\Downloads\\Game.of.Thrones.S07E05.Eastwatch.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT[ettv]\\ --http-password 1234'
-    // cmd.run('vlc --extraintf http --http-src ', directory ,'--http-password 1234');
+  // console.log('vlc ', directory ,' --http-password 1234');
+  // var g = '"D:\Downloads\Game.of.Thrones.S07E05.Eastwatch.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT[ettv]\Game.of.Thrones.S07E05.Eastwatch.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT[ettv].mkv"';
+  // cmd.run('vlc --extraintf http --http-src '+directory+' --http-password 1234');
+  vlcCommand((err, vlcpath) => {
+    console.log(err);
+    console.log('\"'+vlcpath+'\"');
+    if (process.platform === 'win32') {
+    cmd.run('\"'+vlcpath+'\"' + ' --extraintf http --http-password 1234');
+    console.log('win');
+  }
+  else{
+    console.log('mc');
+    cmd.run(vlcpath+ ' --extraintf http --http-password 1234');
+  }
+  })
+
+  //    'vlc --extraintf http --http-src D:\\Downloads\\Game.of.Thrones.S07E05.Eastwatch.1080p.AMZN.WEB-DL.DDP5.1.H.264-GoT[ettv]\\ --http-password 1234'
+  // cmd.run('vlc --extraintf http --http-src ', directory ,'--http-password 1234');
 }
 
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
@@ -46,7 +58,7 @@ var BrowserWindow = require('browser-window');  // Module to create native brows
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
@@ -56,15 +68,15 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 625});
+  mainWindow = new BrowserWindow({ width: 1000, height: 625 });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
